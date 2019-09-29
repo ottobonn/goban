fa55fce0e1e9df97d5f76e7aca90fb5227a354ed
Travis Geis <travis@qualia.com>
2019-09-29 00:33:26 -0600

    Use textures for board details

--- src/Board.js
+++ src/Board.js
@@ -113,6 +113,78 @@ class Board {
     });
   }
 
+  // TODO remove magic scaling 10
+  makeLine(ctx, fromX, fromY, toX, toY, strokeWidth) {
+    ctx.beginPath();
+    ctx.strokeStyle = '#eeeeee';
+    ctx.lineWidth = strokeWidth;
+    ctx.moveTo(fromX * 10, fromY * 10);
+    ctx.lineTo(toX * 10, toY * 10);
+    ctx.stroke();
+  }
+
+  // TODO remove magic scaling 10
+  makeCircle(ctx, {x, y, radius}) {
+    ctx.beginPath();
+    ctx.arc(x * 10, y * 10, radius * 10, 0, 2 * Math.PI);
+    ctx.fillStyle = '#0000ff';
+    ctx.fill();
+  }
+
+  makeTexture() {
+    const {width, height} = this.getDimensions();
+
+    this.canvas = document.createElement('canvas');
+    document.body.appendChild(this.canvas);
+
+    const BITMAP_PIXELS_PER_MM = 10;
+    this.canvas.width = width * BITMAP_PIXELS_PER_MM;
+    this.canvas.height = height * BITMAP_PIXELS_PER_MM;
+
+    const texture = new THREE.Texture(this.canvas);
+    const ctx = this.canvas.getContext('2d');
+    ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    ctx.translate(this.canvas.width / 2, this.canvas.height / 2);
+
+    for (let col = 0; col < this.cols; col++) {
+      const {x: xStart, y: yStart} = this.grid.gridToSceneCoordinates({
+        row: 0,
+        col,
+      });
+      const {x: xEnd, y: yEnd} = this.grid.gridToSceneCoordinates({
+        row: this.rows - 1,
+        col,
+      });
+      this.makeLine(ctx, xStart, yStart, xEnd, yEnd, 1 * BITMAP_PIXELS_PER_MM);
+    }
+
+    for (let row = 0; row < this.rows; row++) {
+      const {x: xStart, y: yStart} = this.grid.gridToSceneCoordinates({
+        row,
+        col: 0,
+      });
+      const {x: xEnd, y: yEnd} = this.grid.gridToSceneCoordinates({
+        row,
+        col: this.cols - 1,
+      });
+      this.makeLine(ctx, xStart, yStart, xEnd, yEnd, 1 * BITMAP_PIXELS_PER_MM);
+    }
+
+    const starPointLocations = this.DIMENSIONS.starPointLocations[this.rows] || [];
+    for (const {row, col} of starPointLocations) {
+      const {x, y} = this.grid.gridToSceneCoordinates({row, col});
+      this.makeCircle(ctx, {
+        x,
+        y,
+        radius: this.DIMENSIONS.starPointRadius,
+      });
+    }
+
+    texture.needsUpdate = true;
+    return texture;
+  }
+
+
   getDimensions() {
     const {
       width: gridWidth,
@@ -126,85 +198,39 @@ class Board {
     };
   }
 
-  makeLine(points) {
-    const geometry = new THREE.Geometry();
-    geometry.vertices = points;
-
-    const material = new MeshLineMaterial({
-      color: 0,
-      lineWidth: 1,
-    });
-
-    const lineObject = new MeshLine();
-    lineObject.setGeometry(geometry, t => 1);
-
-    return new THREE.Mesh(lineObject.geometry, material);
-  }
-
-  makeCircle({x, y, radius}) {
-    const geometry = new THREE.CircleGeometry(this.DIMENSIONS.starPointRadius, 10);
-    geometry.translate(x, y, 0.01);
-    const material = new THREE.MeshBasicMaterial({color: 0xffff00});
-    return new THREE.Mesh(geometry, material);
-  }
-
   makeBoard() {
     const {width, height, depth} = this.getDimensions();
     const geometry = new THREE.BoxGeometry(width, height, depth);
-    geometry.translate(0, 0, -depth / 2 - 10); // top of board is at z = 0
-    const material = new THREE.MeshBasicMaterial({color: 0x9e600b});
-    return new THREE.Mesh(geometry, material);
+    geometry.translate(0, 0, -depth / 2); // top of board is at z = 0
+
+    const topMaterial = new THREE.MeshBasicMaterial({
+      map: this.makeTexture(),
+    });
+
+    const bottomMaterial = new THREE.MeshBasicMaterial({
+      color: '#ff0000',
+    });
+
+    const edgeGrainMaterial = new THREE.MeshBasicMaterial({
+      color: '#00ff00',
+    });
+
+    const endGrainMaterial = new THREE.MeshBasicMaterial({
+      color: '#ff0000',
+    });
+
+    return new THREE.Mesh(geometry, [
+      edgeGrainMaterial,
+      edgeGrainMaterial,
+      endGrainMaterial,
+      endGrainMaterial,
+      topMaterial,
+      bottomMaterial,
+    ]);
   }
 
   getSceneObjects() {
-    const verticalLines = [];
-    const horizontalLines = [];
-
-    for (let col = 0; col < this.cols; col++) {
-      const {x: xStart, y: yStart} = this.grid.gridToSceneCoordinates({
-        row: 0,
-        col,
-      });
-      const {x: xEnd, y: yEnd} = this.grid.gridToSceneCoordinates({
-        row: this.rows - 1,
-        col,
-      });
-      verticalLines.push(this.makeLine([
-        new THREE.Vector3(xStart, yStart, 0),
-        new THREE.Vector3(xEnd, yEnd, 0),
-      ]));
-    }
-
-    for (let row = 0; row < this.rows; row++) {
-      const {x: xStart, y: yStart} = this.grid.gridToSceneCoordinates({
-        row,
-        col: 0,
-      });
-      const {x: xEnd, y: yEnd} = this.grid.gridToSceneCoordinates({
-        row,
-        col: this.cols - 1,
-      });
-      horizontalLines.push(this.makeLine([
-        new THREE.Vector3(xStart, yStart, 0),
-        new THREE.Vector3(xEnd, yEnd, 0),
-      ]));
-    }
-
-    const starPointLocations = this.DIMENSIONS.starPointLocations[this.rows] || [];
-    const starPoints = [];
-    for (const {row, col} of starPointLocations) {
-      const {x, y} = this.grid.gridToSceneCoordinates({row, col});
-      starPoints.push(this.makeCircle({
-        x,
-        y,
-        radius: this.DIMENSIONS.starPointRadius,
-      }));
-    }
-
     return [
-      ...verticalLines,
-      ...horizontalLines,
-      ...starPoints,
       this.makeBoard(),
     ];
   }
